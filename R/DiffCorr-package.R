9152a68fd5d2aa72f1c2bb1dfcc5a8e0e8b39619
Kozo Nishida <kozo.nishida@gmail.com>
2023-08-21 17:57:44 +0900

    Update DiffCorr-package.R

--- R/DiffCorr-package.R
+++ R/DiffCorr-package.R
@@ -1,2 +1,11 @@
-#' @keywords internal 
+#' @title Differential correlations in omics datasets
+#' 
+#' @description
+#'
+#' Analyze and visualize differential correlations in biological networks
+#'
+#' @aliases DiffCorr-package
+#' @name DiffCorr-package
+#' @docType package
+#' @keywords internal
 "_PACKAGE"
