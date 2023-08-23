8577e95bf833e306599dcd516bdc8440bcb0d888
Kozo Nishida <kozo.nishida@gmail.com>
2023-08-23 21:11:44 +0900

    Use new special sentinel _PACKAGE (#10)
    * Create DiffCorr-package.R
    
    * Update DiffCorr.R
    
    * Bump Version
    
    * Update DiffCorr-package.R
    
    * Update DiffCorr-package.R
    
    * Update DESCRIPTION
    
    * Re-generate Rd files with roxygen2

---
+++ R/DiffCorr-package.R
@@ -0,0 +1,15 @@
+#' @title Differential correlations in omics datasets
+#' 
+#' @description
+#'
+#' Analyze and visualize differential correlations in biological networks
+#'
+#' @aliases DiffCorr-package
+#' @name DiffCorr-package
+#' @docType package
+#' @import fdrtool
+#' @import igraph
+#' @import multtest
+#' @import pcaMethods
+#' @keywords internal
+"_PACKAGE"
